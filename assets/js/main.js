7a626be0e31ed50185e2440752047dadc8704031
John Barreto <john.barreto@api-ux.com>
2022-05-27 19:16:27 -0500

    add styles swiper and functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// ########################################################
 // Grab elements
 const selectElement = selector => {
     const element = document.querySelector(selector)
@@ -5,6 +6,7 @@ const selectElement = selector => {
     throw new Error(`Shomething went, make sure that ${selector} exists or is typed correctly`)
 }
 
+// ########################################################
 //Nav styles on scroll
 const scrollHeader = () => {
     const headerElement = selectElement('#header');
@@ -17,6 +19,7 @@ const scrollHeader = () => {
 
 window.addEventListener('scroll', scrollHeader)
 
+// ########################################################
 // Open menu & search pop-up
 const menuToggleIcon = selectElement('#menu-toggle-icon');
 
@@ -29,6 +32,7 @@ const toggleMenu = () => {
 
 menuToggleIcon.addEventListener('click', toggleMenu)
 
+// ########################################################
 // Open/Close search form popup
 const formOpenBtn = selectElement('#search-icon');
 const formCloseBtn = selectElement('#form-close-btn');
@@ -37,18 +41,19 @@ const searchFormContainer = selectElement('#search-form-container');
 formOpenBtn.addEventListener('click', () => searchFormContainer.classList.add('activated'))
 formCloseBtn.addEventListener('click', () => searchFormContainer.classList.remove('activated'))
 
+// -- Close the search form popup on ESC keypress
 window.addEventListener('keyup', event => {
     event.key === "Escape" && searchFormContainer.classList.remove('activated')
-})
-// -- Close the search form popup on ESC keypress
+});
 
+// ########################################################
 // Switch theme/add to local storage
 const bodyElement = document.body;
 const themeToggleBtn = selectElement('#theme-toggle-btn');
 
 const currentTheme = localStorage.getItem('currentTheme');
 
-currentTheme && bodyElement.classList.add('light-theme')
+currentTheme && bodyElement.classList.add('light-theme');
 
 themeToggleBtn.addEventListener('click', () => {
 
@@ -58,5 +63,26 @@ themeToggleBtn.addEventListener('click', () => {
         ? localStorage.setItem('currentTheme', 'themeActive')
         : localStorage.removeItem('currentTheme')
 
-})
+});
+// ########################################################
 // Swiper
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetweenm: 20,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: { el: '.swiper-pagination' },
+    breakpoints: {
+        700: {
+            slidesPerView: 2,
+        },
+        1200: {
+            slidesPerView: 3,
+        },
+        1800: {
+            slidesPerView: 4,
+        }
+    }
+});
